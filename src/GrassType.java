413f1f02c8e38cdcfd936ce20027346713389a84
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 12:12:01 +0800

    Create GrassType.java

---
+++ src/GrassType.java
@@ -0,0 +1,11 @@
+public class GrassType extends Pokemon {
+    public GrassType() {
+        super();
+        setType("Grass");
+    }
+
+    public GrassType(String name, int hp, int att, int def) {
+        super(name, hp, att, def);
+        setType("Grass");
+    }
+}
