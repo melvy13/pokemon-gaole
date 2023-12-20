3dd9750dc892c75c7618fd5739216ac75c9201a2
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 12:10:08 +0800

    Create Pokemon.java

---
+++ src/Pokemon.java
@@ -0,0 +1,56 @@
+public class Pokemon {
+    protected String name, type;
+    protected int hp, att, def;
+
+    // Constructors
+    public Pokemon() {
+        name = "Noname";
+        hp = 100;
+        att = 100;
+        def = 100;
+    }
+
+    public Pokemon(String name, int hp, int att, int def) {
+        this.name = name;
+        this.hp = hp;
+        this.att = att;
+        this.def = def;
+    }
+
+    // Getters & Setters
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getHp() {
+        return hp;
+    }
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    public int getAtt() {
+        return att;
+    }
+    public void setAtt(int att) {
+        this.att = att;
+    }
+    public int getDef() {
+        return def;
+    }
+    public void setDef(int def) {
+        this.def = def;
+    }
+    public String getType() {
+        return type;
+    }
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s [%s] HP: %d Attack: %d Defense: %d", name, type, hp, att, def);
+    }
+}
