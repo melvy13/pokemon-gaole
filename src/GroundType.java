23994dd3c17f52bf2d4630d78939a5eff8a1aa6b
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 12:14:26 +0800

    Create GroundType.java

---
+++ src/GroundType.java
@@ -0,0 +1,11 @@
+public class GroundType extends Pokemon {
+    public GroundType() {
+        super();
+        setType("Ground");
+    }
+
+    public GroundType(String name, int hp, int att, int def) {
+        super(name, hp, att, def);
+        setType("Ground");
+    }
+}
