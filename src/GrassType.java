17f1e23840066cd7808634cc7f5dc2f0a4ea15b4
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 20:33:56 +0800

    Update GrassType.java

--- src/GrassType.java
+++ src/GrassType.java
@@ -8,4 +8,17 @@ public class GrassType extends Pokemon {
         super(name, hp, att, def);
         setType("Grass");
     }
+
+    public double effectiveness(String enemyType) {
+        double effectiveness = 1;
+        if (enemyType == "Fire") {
+            effectiveness = 2;
+        }
+
+        else if (enemyType == "Water" || enemyType == "Grass" || enemyType == "Electric" || enemyType == "Ground") {
+            effectiveness = 0.5;
+        }
+
+        return effectiveness;
+    }
 }
