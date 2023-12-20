79d5a291859ca9e1ff86e57bb3717bfc238862d3
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 12:11:00 +0800

    Create FireType.java

---
+++ src/FireType.java
@@ -0,0 +1,11 @@
+public class FireType extends Pokemon {
+    public FireType() {
+        super();
+        setType("Fire");
+    }
+
+    public FireType(String name, int hp, int att, int def) {
+        super(name, hp, att, def);
+        setType("Fire");
+    }
+}
