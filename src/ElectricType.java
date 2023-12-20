060536859ad9d46af8fe7075652e82f5285911f7
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 12:13:35 +0800

    Create ElectricType.java

---
+++ src/ElectricType.java
@@ -0,0 +1,11 @@
+public class ElectricType extends Pokemon {
+    public ElectricType() {
+        super();
+        setType("Electric");
+    }
+
+    public ElectricType(String name, int hp, int att, int def) {
+        super(name, hp, att, def);
+        setType("Electric");
+    }
+}
