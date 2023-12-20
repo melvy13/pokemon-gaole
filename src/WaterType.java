f51bce7178c78f916b2631d9707cea8fa11e7212
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-20 12:11:38 +0800

    Create WaterType.java

---
+++ src/WaterType.java
@@ -0,0 +1,11 @@
+public class WaterType extends Pokemon {
+    public WaterType() {
+        super();
+        setType("Water");
+    }
+
+    public WaterType(String name, int hp, int att, int def) {
+        super(name, hp, att, def);
+        setType("Water");
+    }
+}
