0087c50f307d7c350aec71f9de44c9ed55dd6872
melvy13 <134412174+melvy13@users.noreply.github.com>
2023-12-22 11:15:50 +0800

    Update Battle.java

--- src/Battle.java
+++ src/Battle.java
@@ -10,10 +10,10 @@ public class Battle {
     private String results;
     Random rand = new Random();
     Scanner input = new Scanner(System.in);
-	
+
     private ArrayList<Pokemon> pool = Game.pool;
 
-	public Pokemon getWild1() {
+    public Pokemon getWild1() {
         return wild1;
     }
 
@@ -24,7 +24,7 @@ public class Battle {
     public String getResults() {
         return results;
     }
-	
+
     // Battle flow - assign results as "win" or "lose"
     public void startBattle(ArrayList<Pokemon> playerPokemons) {
         displayWildPokemons();
@@ -49,7 +49,7 @@ public class Battle {
 
     // Display 2 random wild Pokemons from pool
     private void displayWildPokemons() {
-        int i1 = rand.nextInt(pool.size());
+        int i1 = rand.nextInt(Game.pool.size());
 		int i2;
         do {
 			i2 = rand.nextInt(pool.size());
@@ -61,31 +61,44 @@ public class Battle {
 
     // Choose 2 Pokemons from player Pokemons list (or rent a Pokemon)
     private void choosePokemons(ArrayList<Pokemon> playerPokemons) {
-        System.out.println("You currently have these Pokemons!: ");
-        for (int i = 0; i < playerPokemons.size(); i++) {
-            System.out.printf("[%d] %s\n", i+1, playerPokemons.get(i));
-        }
+        while (true) {
+            try {
+                System.out.println("You currently have these Pokemons!: ");
 
-        // If playerPokemons has only 1 Pokemon - Rent a random one from the pool
-        if (playerPokemons.size() < 2) {
-            System.out.println("You don't have enough Pokemons! You need to rent a Pokemon!");
-            int rent = rand.nextInt(pool.size());
-            System.out.printf("You have randomly rented a %s!\n", pool.get(rent).getName());
-			System.out.printf("You sent out %s & %s!\n", playerPokemons.get(0).getName(), pool.get(rent).getName());
-            pokemon1 = playerPokemons.get(0);
-            pokemon2 = pool.get(rent);
-        }
+                for (int i = 0; i < playerPokemons.size(); i++) {
+                System.out.printf("[%d] %s\n", i+1, playerPokemons.get(i).getName());
+                }
 
-        // Else choose 2 Pokemons from playerPokemons to send out
-        else {
-            System.out.print("Choose your first Pokemon!: ");
-			int choice1 = input.nextInt();
-			System.out.print("Choose your second Pokemon!: ");
-			int choice2 = input.nextInt();
-            System.out.printf("You sent out %s & %s!\n", playerPokemons.get(choice1-1).getName(), playerPokemons.get(choice2-1).getName());
-            pokemon1 = playerPokemons.get(choice1-1);
-            pokemon2 = playerPokemons.get(choice2-1);
+                // If playerPokemons has only 1 Pokemon - Rent a random one from the pool
+                if (playerPokemons.size() < 2) {
+                    System.out.println("You don't have enough Pokemons! You need to rent a Pokemon!");
+                    int rent = rand.nextInt(pool.size());
+                    System.out.printf("You have randomly rented a %s!\n", pool.get(rent).getName());
+                    System.out.printf("You sent out %s & %s!\n", playerPokemons.get(0).getName(), pool.get(rent).getName());
+                    pokemon1 = playerPokemons.get(0);
+                    pokemon2 = pool.get(rent);
+                }
+
+                // Else choose 2 Pokemons from playerPokemons to send out
+                else {
+                    System.out.print("Choose your first Pokemon!: ");
+                    int choice1 = input.nextInt();
+                    System.out.print("Choose your second Pokemon!: ");
+                    int choice2 = input.nextInt();
+                    System.out.printf("You sent out %s & %s!\n", playerPokemons.get(choice1-1).getName(), playerPokemons.get(choice2-1).getName());
+                    pokemon1 = playerPokemons.get(choice1-1);
+                    pokemon2 = playerPokemons.get(choice2-1);
+                }
+            }
+
+            catch (IndexOutOfBoundsException e) {
+                System.out.println("Invalid options!");
+                continue;
+            }
+            
+            break;
         }
+        
     }
 
     // Determine who attacks first
@@ -94,7 +107,7 @@ public class Battle {
     // 3. User input a random number
     // 4. Whoever's random number is closer to the hidden number will attack first
     private String battleSequence() {
-		System.out.println("\nA hidden number from 1-100 is generated. Whoever's input number is closer to the hidden number will attack!");
+        System.out.println("\nA hidden number from 1-100 is generated. Whoever's input number is closer to the hidden number will attack!");
         int hiddenNo = rand.nextInt(1, 101);
         System.out.print("Input a random number from 1-100!: ");
         int userNo = input.nextInt();
@@ -114,33 +127,33 @@ public class Battle {
 
             if (enemyDiff < userDiff) {
                 System.out.println("Enemy's number is closer to the hidden number!");
-                System.out.println("Enemy will attack!");
-                attacking = "enemy";
+                System.out.println("Enemy will attack!\n");
+                attacker = "enemy";
                 break;
             }
 
             else {
                 System.out.println("Your number is closer to the hidden number!");
-                System.out.println("You will attack!");
-                attacking = "user";
+                System.out.println("You will attack!\n");
+                attacker = "user";
                 break;
             }
         }
         return attacker;
     }
 
-	// Calculate damage
-	private int calculateDamage(Pokemon attacker, Pokemon defender) {
-		int att = attacker.getAtt();
-		int def = defender.getDef();
-		int level = attacker.getLevel();
-		double effectiveness = defender.effectiveness(attacker.getType());       
-		
-		double damage = ((0.4 * level + 2) * att / def + 2) * effectiveness * rand.nextDouble(0.85, 1.00);
-		return (int)damage;
+    // Calculate damage
+    private int calculateDamage(Pokemon attacker, Pokemon defender) {
+        int att = attacker.getAtt();
+        int def = defender.getDef();
+        int level = attacker.getLevel();
+        double effectiveness = defender.effectiveness(attacker.getType());       
+
+        double damage = ((0.4 * level + 2) * att / def + 2) * effectiveness * rand.nextDouble(0.85, 1.00);
+        return (int)damage;
     }
 
-	private void attack(String attacker) {
+    private void attack(String attacker) {
         if (attacker.equals("user")) {
             attack(pokemon1, pokemon2, wild1, wild2);
         } else if (attacker.equals("enemy")) {
@@ -148,7 +161,7 @@ public class Battle {
         }
     }
 
-	// Attacking
+    // Attacking
     private void attack(Pokemon att1, Pokemon att2, Pokemon def1, Pokemon def2) {
         System.out.printf("%s: %d HP ; %s: %d HP\n", def1.getName(), def1.getHp(), def2.getName(), def2.getHp());
         if (att1.getHp() > 0 && att2.getHp() > 0) {
